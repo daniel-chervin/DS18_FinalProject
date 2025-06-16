b9d85bdf1d21954ab883b60637b903dcdfe91713
daniel-chervin <daniel.chervin@gmail.com>
2025-06-16 12:53:17 +0300

    speed up through caching volumes

--- 3D_UNET Segmentation/3D_UNet_Tumor_Segmentation_Streamlit.py
+++ 3D_UNET Segmentation/3D_UNet_Tumor_Segmentation_Streamlit.py
@@ -43,53 +43,72 @@ cases = sorted(cases)
 
 st.title('Interactive Brain Tumor Segmentation Viewer')
 
+# Cache loading of all volumes into memory
+@st.cache_data
+def load_all_volumes(input_dir, pred_dir, gt_dir):
+    cases = [f.replace('_T1.nii.gz', '') for f in os.listdir(input_dir) if f.endswith('_T1.nii.gz')]
+    cases = sorted(cases)
+    mri_dict = {}
+    pred_dict = {}
+    gt_dict = {}
+    for case in cases:
+        # Load once and store arrays of shape [Z, Y, X]
+        img_path  = os.path.join(input_dir, f"{case}_T1.nii.gz")
+        pred_path = os.path.join(pred_dir,  f"{case}_predict_seg.nii.gz")
+        gt_path   = os.path.join(gt_dir,    f"{case}.nii.gz")
+        mri = sitk.ReadImage(img_path)
+        pred = sitk.ReadImage(pred_path)
+        gt = sitk.ReadImage(gt_path)
+        mri_dict[case]  = sitk.GetArrayFromImage(mri)
+        pred_dict[case] = sitk.GetArrayFromImage(pred)
+        gt_dict[case]   = sitk.GetArrayFromImage(gt)
+    return cases, mri_dict, pred_dict, gt_dict
+
+# Load all volumes once
+cases, mri_vols, pred_vols, gt_vols = load_all_volumes(INPUT_DIR, PRED_DIR, GT_DIR)
+
 # Sidebar controls
 st.sidebar.header('Controls')
 selected_case = st.sidebar.selectbox('Case', cases)
 plane = st.sidebar.selectbox('Plane', ['axial', 'sagittal', 'coronal'])
 
-# Load volumes
-@st.cache(allow_output_mutation=True)
-def load_volume(dir_path, base, suffix):
-    path = os.path.join(dir_path, f"{base}{suffix}")
-    img = sitk.ReadImage(path)
-    return sitk.GetArrayFromImage(img)
-
-img_vol = load_volume(INPUT_DIR, selected_case, '_T1.nii.gz')
-pred_vol = load_volume(PRED_DIR,  selected_case, '_predict_seg.nii.gz')
-gt_vol   = load_volume(GT_DIR,    selected_case, '.nii.gz')
-
-# Determine slice max index
+# Determine slice range based on plane
+vol_shape = mri_vols[selected_case].shape
 if plane == 'axial':
-    max_idx = img_vol.shape[0]
+    max_idx = vol_shape[0]
 elif plane == 'sagittal':
-    max_idx = img_vol.shape[2]
-else:
-    max_idx = img_vol.shape[1]
-
-slice_idx = st.sidebar.slider('Slice', min_value=0, max_value=max_idx-1, value=max_idx//2)
-
-# Extract slice
-if plane == 'axial':
-    img_sl = img_vol[slice_idx, :, :]
-    pred_sl= pred_vol[slice_idx, :, :]
-    gt_sl  = gt_vol[slice_idx, :, :]
-elif plane == 'sagittal':
-    img_sl = img_vol[:, :, slice_idx]
-    pred_sl= pred_vol[:, :, slice_idx]
-    gt_sl  = gt_vol[:, :, slice_idx]
+    max_idx = vol_shape[2]
 else:  # coronal
-    img_sl = img_vol[:, slice_idx, :]
-    pred_sl= pred_vol[:, slice_idx, :]
-    gt_sl  = gt_vol[:, slice_idx, :]
+    max_idx = vol_shape[1]
+slice_idx = st.sidebar.slider('Slice', 0, max_idx - 1, max_idx // 2)
 
-# Plot
+# Extract selected slices
+img_sl  = (mri_vols[selected_case][slice_idx, :, :] if plane == 'axial'
+           else mri_vols[selected_case][:, :, slice_idx] if plane == 'sagittal'
+           else mri_vols[selected_case][:, slice_idx, :])
+pred_sl = (pred_vols[selected_case][slice_idx, :, :] if plane == 'axial'
+           else pred_vols[selected_case][:, :, slice_idx] if plane == 'sagittal'
+           else pred_vols[selected_case][:, slice_idx, :])
+gt_sl   = (gt_vols[selected_case][slice_idx, :, :] if plane == 'axial'
+           else gt_vols[selected_case][:, :, slice_idx] if plane == 'sagittal'
+           else gt_vols[selected_case][:, slice_idx, :])
+
+# Plot three views
 fig, axes = plt.subplots(3, 1, figsize=(6, 12))
-axes[0].imshow(img_sl, cmap='gray'); axes[0].set_title('MRI'); axes[0].axis('off')
-axes[1].imshow(img_sl, cmap='gray'); axes[1].imshow(pred_sl, cmap='jet', alpha=0.5);
-axes[1].set_title('Prediction'); axes[1].axis('off')
-axes[2].imshow(img_sl, cmap='gray'); axes[2].imshow(gt_sl, cmap='jet', alpha=0.5);
-axes[2].set_title('Ground Truth'); axes[2].axis('off')
+axes[0].imshow(img_sl, cmap='gray')
+axes[0].set_title('MRI')
+axes[0].axis('off')
+
+axes[1].imshow(img_sl, cmap='gray')
+axes[1].imshow(pred_sl, cmap='jet', alpha=0.5)
+axes[1].set_title('Prediction')
+axes[1].axis('off')
+
+axes[2].imshow(img_sl, cmap='gray')
+axes[2].imshow(gt_sl, cmap='jet', alpha=0.5)
+axes[2].set_title('Ground Truth')
+axes[2].axis('off')
+
 plt.tight_layout()
 
 st.pyplot(fig)
