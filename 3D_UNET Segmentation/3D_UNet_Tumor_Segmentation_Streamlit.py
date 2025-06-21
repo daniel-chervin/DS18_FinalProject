5fd0a8378ad168d37cf1d7cb0af67b83e4e2135f
daniel-chervin <daniel.chervin@gmail.com>
2025-06-21 22:59:35 +0300

    tests

--- 3D_UNET Segmentation/3D_UNet_Tumor_Segmentation_Streamlit.py
+++ 3D_UNET Segmentation/3D_UNet_Tumor_Segmentation_Streamlit.py
@@ -179,7 +179,7 @@ with st.spinner("Calling GPT..."):
     #st.write(f'st.session_state.analysis_md is {st.session_state.get("analysis_md")}')
     if st.session_state.get("analysis_md") is None:
         #st.write('st.session_state.get("analysis_md") is None, calling GPT!')
-        st.session_state.analysis_md = analyze_metrics_with_gpt(metrics) # random.randint(1, 1000)
+        st.session_state.analysis_md = random.randint(1, 1000) # analyze_metrics_with_gpt(metrics) #
     #else:
     #    st.write("Reuse analysis_md.")
 
